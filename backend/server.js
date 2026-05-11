b9a8dc1c4bd77f84f379b68675714154d31196e8
Shivin Pruthi <pruthi.shivin30@gmail.com>
2026-05-11 22:38:50 +0530

    Fixed server.js

--- backend/server.js
+++ backend/server.js
@@ -10,9 +10,7 @@ connectDB();
 
 const app = express();
 
-app.use(cors({
-  origin: "http://localhost:5173"
-}));
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/tasks", require("./routes/taskRoutes"));
